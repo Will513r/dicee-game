0f0b6811ccb6e75878941049823571828a9f55be
William Ray <will513r@gmail.com>
2024-05-11 12:54:04 -0400

    updated JS

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomImage = "dice" + randomNumber1 + ".png";
 var randomImageSource = "images/" + randomImage;
-
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
@@ -9,7 +8,6 @@ image1.setAttribute("src", randomImageSource);
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImage2 = "dice" + randomNumber2 + ".png";
 var randomImageSource2 = "images/" + randomImage2;
-
 var image2 = document.querySelectorAll("img")[1];
 
 image2.setAttribute("src", randomImageSource2);
